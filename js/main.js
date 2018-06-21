91ab211a25d7ae1ef2daf4675eba8227fcf40738
Maksim <31015359+MaxMorozov23@users.noreply.github.com>
2018-06-21 13:31:20 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ var randomPhrase = new Vue({
   data: {
     counter: 0,
     phrases: [
-      'М’Айк устал. Приставай к кому-нибудь другому.',
-      'М’Айк ищет коловианский меховой шлем. Практичный и стильный. М’Айк очень опечален, что у него нет такого шлема.',
-      'М’Айк хотел бы иметь рыбную палочку, чтобы подарить ее тебе. К сожалению, у него ее нет.',
-      'М’Айк верит, что дети наше будущее. Но он не хочет, чтобы они портили нам удовольствие.'
-
+      "M’aiq's father was also called M'aiq. As was M'aiq's father's father. At least, that's what his father said.",
+      "M’aiq wishes you well.",
+      "M’aiq knows much, and tells some. M'aiq knows many things others do not.",
+      "M’aiq carries two weapons, to be safe. What if one breaks? That would be most unlucky.",
+      "Feet are for walking. Hands are for hitting. Or shaking. Or waving. Sometimes for clapping.",
+      "I do not wish to fight on horseback. It is a good way to ruin a perfectly good horse... which is, to say, a perfectly good dinner.",
+      "I don't know why one would want to destroy a building. It takes time to make it. Much time."
     ],
     selectedPhrase: ''
   },
@@ -15,6 +17,10 @@ var randomPhrase = new Vue({
     newPhrase: function () {
       const randomNumber = Math.floor(Math.random() * this.phrases.length)
       this.selectedPhrase = this.phrases[randomNumber]
+      this.counter++;
+      if (this.counter > 4) {
+        this.selectedPhrase = "M'aiq is tired now. Go bother somebody else."
+      }
     }
   }
 })
